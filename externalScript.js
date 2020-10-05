6b178be09060aae0c1991de93caa6c3ef98e8f54
markevillive <mark_evillive@hotmail.com>
2020-10-06 00:26:21 +0700

    variables

--- externalScript.js
+++ externalScript.js
@@ -1,15 +1,9 @@
+//Строчный комментарий
+document.write('<h2>Hello, JavaScript!</h2>');
 /*
-if(confirm('Вы согласны нажать на эту кнопку?')){
-    document.write('<h1>Спасибо за согласие!</h1>');
-}else {
-    document.write('<h4>Извините за беспокойство</h4>');
-}
-*/
-
-var ansver = prompt('Вы согласны нажать на эту кнопку?');
-
-if(ansver){
-    console.log(ansver);
-}else {
-    console.log('Извините за беспокойство');
-}
+Блочный комментарий
+ */
+var footwearSize = 42;
+var footwearLabel = 'Converse';
+document.write(footwearLabel);
+document.write(footwearSize);
